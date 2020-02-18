041016e6c8ea8584234c5afc2a463137af4dc486
dtirnanic <dejan.tirnanic85@gmail.com>
2020-02-18 13:23:20 -0600

    Models created for Customer, Account, Employee, Address and populated with properties

---
+++ Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollector.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        public int AddressId { get; set; }
+        public int AccountId { get; set; }
+        public int ApplicationUser { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
